78199c3cb3cb991df8b311d842dd91d0d2eaa4ad
DustinMcGilvray <15370964+DustinMcGilvray@users.noreply.github.com>
2018-08-29 13:46:50 -0500

    Fixed Bug. Table and Propmt Questions now populating terminal window.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,52 +12,68 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-//Prompt User Function
-function userPrompt() {
-    inquirer.prompt([
-        {
-            name: "item_id",
-            type:"input",
-            message: "Enter the Item ID number of the product you wish to purchase."
-        },
-        {
-            name:"stock_quantity",
-            type: "input",
-            message: "How many would you like?"
-        }
-    ])
-    .then(function(answer) {
-        var checkData = "SELECT * FROM products Where ?"
-        connection.query(checkData, {answer:item_id}, function(error, response) {
-            if (error) throw error;
-        }
-);
+//Display Current Inventory after Connection
+connection.connect(function (error) {
+    if (error) throw error;
+    startApp();
 });
 
+
+function startApp() {
+    displayItems();
+}
+
+
+//Prompt User Function
+function userPrompt() {
+    inquirer.prompt([{
+                name: "item_id",
+                type: "input",
+                message: "Enter the Item ID number of the product you wish to purchase."
+            },
+            {
+                name: "stock_quantity",
+                type: "input",
+                message: "How many would you like?"
+            }
+        ])
+        .then(function (answer) {
+            var checkData = "SELECT stock_quantity FROM products Where ?"
+            connection.query(checkData, {
+                answer: item_id
+            }, function (error, response) {
+                if (error) throw error;
+                if (response.length === 0) {
+                    console.log
+                }
+            });
+        });
+}
+
 //Display mqSQL Database in Terminal Window
-function currentInventory() {
+function displayItems() {
     var displayData = "SELECT * FROM products";
     connection.query(displayData, function (error, response) {
-            if (error) throw error;
-            console.log("\n\n");
+        if (error) throw error;
+        console.log("\n\n");
 
-            //Formatting Database Table in the Terminal Screen
-            var t = new Table({
-                borderStyle: 3,
-                horizontalLine: true,
-                width: [5, "40%", "10%", 10, 5]
-            }, {attrs: "center"});
-           
-            t.push(["ID", "Product", "Price"]);
+        //Formatting Database Table in the Terminal Screen
+        var t = new Table({
+            borderStyle: 3,
+            horizontalLine: true,
+            width: [5, "40%", "10%", 10, 5]
+        }, {
+            attrs: "center"
+        });
 
-            response.forEach(function (Items) {
-                t.push([Items.id, Items.product_name, Items.department_name, "$"+Items.price, Items.stock_quantity])
-            });
+        t.push(["ID", "Product", "Price"]);
 
-            console.log(" " + t);
+        response.forEach(function (Items) {
+            t.push([Items.id, Items.product_name, "$" + Items.price])
+        });
 
-            userPrompt();
-        }
-    )};
-
-    // currentInventory();
+        console.log(" " + t);
+        userPrompt();
+    });
+    
+    }
