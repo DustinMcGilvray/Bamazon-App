be4783b241a71b570a4feb55b99e060a39399dcc
DustinMcGilvray <15370964+DustinMcGilvray@users.noreply.github.com>
2018-09-01 10:19:11 -0500

    Updated Files.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("terminal-table");
-
 //Setting up the mySQL Connection to Database
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,13 +10,11 @@ var connection = mysql.createConnection({
     password: "Password",
     database: "bamazon_db"
 });
-
 //Display Current Inventory after Connection
 connection.connect(function (error) {
     if (error) throw error;
     startApp();
 });
-
 //App Start Function
 function startApp() {
     inquirer.prompt({
@@ -46,7 +43,6 @@ function startApp() {
         }
     });
 }
-
 //Prompt User Function
 function userPrompt() {
     inquirer.prompt([{
@@ -76,7 +72,7 @@ function userPrompt() {
                     console.log("We are processing your order.");
 
                     // Update the mySQL Database
-                    var updateQty = "UPDATE products SET ? WHERE ?"
+                    var updateQty = "UPDATE products SET ? WHERE ?";
                     connection.query(updateQty, [
                         {
                             stock_quantity:response[0].stock_quantity-userQty
@@ -89,11 +85,12 @@ function userPrompt() {
                         // console.log(data);
                         console.log("Your order has been processed. Your total is $" + response[0].price * userQty);
                     })
+                    connection.end();
                 }
             });
         });
+        
 }
-
 //Display All Items in mqSQL Database in Terminal Window
 function allItems() {
     var displayData = "SELECT * FROM products";
@@ -102,19 +99,32 @@ function allItems() {
         console.log("\n\n");
 
         //Formatting Database Table in the Terminal Screen
-        var t = new Table({
-            borderStyle: 2,
+        var t = new Table ({
+            borderStyle: 3,
             horizontalLine: true,
-            width: [5, 40, 5]
+            width: [5, 60, 15]
         });
 
         t.push(["ID", "Product", "Price"]);
+        t.attrRange({row: [0, 1]}, {
+            align: "center",
+            color: "blue",
+            bg: "black"
+          });
+          t.attrRange({row: [1], column: [0,3]}, {
+            color: "green",
+            bg: "black"
+          });
+          t.attrRange({column: [0,1]},
+        {align: "center"});
+        t.attrRange({column: [2]},
+            {align: "right"});
 
         response.forEach(function (Items) {
             t.push([Items.id, Items.product_name, "$" + Items.price])
         });
 
-        console.log(" " + t);
+        console.log("" + t);
         userPrompt();
     });
 }
@@ -124,21 +134,31 @@ function apparelItems() {
     connection.query(displayData,
         function (error, response) {
            for (var i = 0; i < response.length; i++) {
-            //    console.log(response[i].id + response[i].product_name + response[i].price);
            }
         if (error) throw error;
         console.log("\n");
 
         //Formatting Database Table in the Terminal Screen
-        var t = new Table({
+        var t = new Table ({
             borderStyle: 3,
             horizontalLine: true,
-            width: [5, 40, 5]
-        }, {
-            attrs: "center"
+            width: [5, 60, 15],
         });
 
-        t.push(["ID", "Product", "Price"]);
+         t.push(["ID", "Product", "Price"]);
+        t.attrRange({row: [0, 1]}, {
+            align: "center",
+            color: "blue",
+            bg: "black"
+          });
+          t.attrRange({row: [1], column: [0,3]}, {
+            color: "green",
+            bg: "black"
+          });
+          t.attrRange({column: [0,1]},
+        {align: "center"});
+        t.attrRange({column: [2]},
+            {align: "right"});
 
         response.forEach(function (Items) {
             t.push([Items.id, Items.product_name, "$" + Items.price])
@@ -150,18 +170,25 @@ function apparelItems() {
 }
 //Display Toy Items in mqSQL Database in Terminal Window
 function toyItems() {
-    var displayData = "SELECT department_name:Toys FROM products WHERE ?";
-    connection.query(displayData, function (error, response) {
+    var displayData = "SELECT id, product_name, price FROM products WHERE department_name= 'Toys'";
+    connection.query(displayData,
+        function (error, response) {
+           for (var i = 0; i < response.length; i++) {
+           }
         if (error) throw error;
-        console.log("\n\n");
+        console.log("\n");
 
         //Formatting Database Table in the Terminal Screen
-        var t = new Table({
+        var t = new Table ({
             borderStyle: 3,
             horizontalLine: true,
-            width: [5, 40, 5]
-        }, {
-            attrs: "center"
+            width: [3],
+            border:{
+                sep:"U2551",
+                topLeft:"U2554", topmid: "U2566", top:"U2550", topRight:"U2557",
+                midLeft:"U2560", midMid:"U256C", mid:"U2550", midRight:"U2563",
+                botLeft:"U255A", botMid:"U2569", bot:"U2550", botRight:'U255D'
+            }
         });
 
         t.push(["ID", "Product", "Price"]);
@@ -176,18 +203,25 @@ function toyItems() {
 }
 //Display Movie Items in mqSQL Database in Terminal Window
 function movieItems() {
-    var displayData = "SELECT department_name:Movies FROM products WHERE ?";
-    connection.query(displayData, function (error, response) {
+    var displayData = "SELECT id, product_name, price FROM products WHERE department_name= 'Movies'";
+    connection.query(displayData,
+        function (error, response) {
+           for (var i = 0; i < response.length; i++) {
+           }
         if (error) throw error;
-        console.log("\n\n");
+        console.log("\n");
 
         //Formatting Database Table in the Terminal Screen
-        var t = new Table({
+        var t = new Table ({
             borderStyle: 3,
             horizontalLine: true,
-            width: [5, 40, 5]
-        }, {
-            attrs: "center"
+            width: [3],
+            border:{
+                sep:"U2551",
+                topLeft:"U2554", topmid: "U2566", top:"U2550", topRight:"U2557",
+                midLeft:"U2560", midMid:"U256C", mid:"U2550", midRight:"U2563",
+                botLeft:"U255A", botMid:"U2569", bot:"U2550", botRight:'U255D'
+            }
         });
 
         t.push(["ID", "Product", "Price"]);
@@ -202,18 +236,25 @@ function movieItems() {
 }
 //Display Music Items in mqSQL Database in Terminal Window
 function musicItems() {
-    var displayData = "SELECT department_name:Music FROM products WHERE ?";
-    connection.query(displayData, function (error, response) {
+    var displayData = "SELECT id, product_name, price FROM products WHERE department_name= 'Music'";
+    connection.query(displayData,
+        function (error, response) {
+           for (var i = 0; i < response.length; i++) {
+           }
         if (error) throw error;
-        console.log("\n\n");
+        console.log("\n");
 
         //Formatting Database Table in the Terminal Screen
-        var t = new Table({
+        var t = new Table ({
             borderStyle: 3,
             horizontalLine: true,
-            width: [5, 40, 5]
-        }, {
-            attrs: "center"
+            width: [3],
+            border:{
+                sep:"U2551",
+                topLeft:"U2554", topmid: "U2566", top:"U2550", topRight:"U2557",
+                midLeft:"U2560", midMid:"U256C", mid:"U2550", midRight:"U2563",
+                botLeft:"U255A", botMid:"U2569", bot:"U2550", botRight:'U255D'
+            }
         });
 
         t.push(["ID", "Product", "Price"]);
---
+++ bamazonManager.js
@@ -0,0 +1,212 @@
+//Requiring npm's
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("terminal-table");
+//Setting up the mySQL Connection to Database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Password",
+    database: "bamazon_db"
+});
+//Display Current Inventory after Connection
+connection.connect(function (error) {
+    if (error) throw error;
+    startApp();
+});
+//App Start Function
+function startApp() {
+    inquirer.prompt({
+            name: "search",
+            type: "list",
+            message: "Choose a Menu Option to Continue.",
+            choices: [
+                "View Products for Sale.",
+                "View Low Inventory.",
+                "Add to Inventory",
+                "Add New Product"
+            ]
+        })
+        .then(function (answer) {
+            if (answer.search === "View Products for Sale.") {
+                saleProducts();
+            } else if (answer.search === "View Low Inventory.") {
+                lowInventory();
+            } else if (answer.search === "Add to Inventory") {
+                addInventory();
+            } else if (answer.search === "Add New Product") {
+                newProduct();
+            }
+        });
+}
+//End Connection Function
+function endConnection() {
+    inquirer.prompt({
+            name: "end",
+            type: "list",
+            message: "Are you ready to end the connection?",
+            choices: [
+                "YES",
+                "NO"
+            ]
+        })
+        .then(function (answer) {
+            if (answer.end === "YES") {
+                connection.end();
+            } else if (answer.end === "NO") {
+                startApp();
+            }
+        });
+}
+// Function for Viewing Products for Sale
+function saleProducts() {
+    var displayData = "SELECT * FROM products";
+    connection.query(displayData, function (error, response) {
+        if (error) throw error;
+        console.log("\n\n");
+
+        //Formatting Database Table in the Terminal Screen
+        var t = new Table ({
+            borderStyle: 3,
+            horizontalLine: true,
+            width: [3],
+            border:{
+                sep:"U2551",
+                topLeft:"U2554", topmid: "U2566", top:"U2550", topRight:"U2557",
+                midLeft:"U2560", midMid:"U256C", mid:"U2550", midRight:"U2563",
+                botLeft:"U255A", botMid:"U2569", bot:"U2550", botRight:'U255D'
+            }
+        });
+
+        t.push(["ID", "Product", "Price", "Stock Quantity"]);
+
+        response.forEach(function (Items) {
+            t.push([Items.id, Items.product_name, "$" + Items.price, Items.stock_quantity])
+        });
+
+        console.log(" " + t);
+        endConnection();
+    });
+}
+// Function for Viewing Low Inventory
+function lowInventory() {
+    var displayData = "SELECT * FROM products WHERE stock_quantity < 5";
+    connection.query(displayData, function (error, response) {
+        if (error) throw error;
+        console.log("\n\n");
+        console.log("Low Inventory Levels");
+        //Formatting Database Table in the Terminal Screen
+        var t = new Table ({
+            borderStyle: 3,
+            horizontalLine: true,
+            width: [3],
+            border:{
+                sep:"U2551",
+                topLeft:"U2554", topmid: "U2566", top:"U2550", topRight:"U2557",
+                midLeft:"U2560", midMid:"U256C", mid:"U2550", midRight:"U2563",
+                botLeft:"U255A", botMid:"U2569", bot:"U2550", botRight:'U255D'
+            }
+        });
+
+        t.push(["ID", "Product", "Price", "Stock Quantity"]);
+
+        response.forEach(function (Items) {
+            t.push([Items.id, Items.product_name, "$" + Items.price, Items.stock_quantity])
+        });
+
+        console.log(" " + t);
+        endConnection();
+    });
+}
+//Function for Adding to Inventory Levels
+function addInventory() {
+    inquirer.prompt([{
+                name: "id",
+                type: "input",
+                message: "Please input the id for the product you wish to update.",
+            },
+            {
+                name: "stock",
+                type: "input",
+                message: "How much stock would you like to add?"
+            }
+        ])
+        .then(function (answer) {
+            var userItem = answer.id;
+            var userQty = parseInt(answer.stock);
+            var sql = "SELECT * FROM products Where ?";
+            // console.log("userQty", userQty);
+            connection.query(sql, {
+                id: userItem
+            }, function (error, response) {
+                // console.log(response);
+                // console.log(response[0].stock_quantity)
+                if (error) throw error;
+
+                if (response[0].stock_quantity + userQty) {
+                    console.log("Stock quantity is being updated.");
+
+                    // Update the mySQL Database
+                    var updateQty = "UPDATE products SET ? WHERE ?";
+                    connection.query(updateQty, [{
+                            stock_quantity: response[0].stock_quantity + userQty
+                        },
+                        {
+                            id: userItem
+                        }
+                    ], function (error, data) {
+                        if (error) throw error;
+                        console.log("Stock has been updated. You have added " + answer.stock);
+                        endConnection();
+                    })
+                }
+            });
+           
+        });
+}
+//Function for Adding a New Product
+function newProduct() {
+    inquirer.prompt([{
+                name: "product",
+                type: "input",
+                message: "What is the Product Name?",
+            },
+            {
+                name: "department",
+                type: "list",
+                message: "What Department will the product be located in?",
+                choices: [
+                    "Apparel",
+                    "Toys",
+                    "Movies",
+                    "Music"
+                ]
+            },
+            {
+                name: "price",
+                type: "input",
+                message: "What is the Sale Price of the new product?",
+            },
+            {
+                name: "stock",
+                type: "input",
+                message: "What is the stock quantity of the new product?",
+            }
+        ])
+        .then(function (answer) {
+            var sql = "INSERT INTO products SET ?";
+            connection.query( sql,
+                {
+                product_name:answer.product,
+                department_name:answer.department,
+                price:answer.price,
+                stock_quantity:answer.stock
+                }, 
+                function(error, response) {
+                    console.log("The new product has been added!");
+                    endConnection();
+                }
+        );
+        })
+}
